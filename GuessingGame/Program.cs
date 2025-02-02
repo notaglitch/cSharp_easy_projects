4b7f809df7832991610ac55037b12d16559fb83e
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-02 21:30:30 +0100

    Added GuessingGame

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+
+            string guess = "";
+            while (guess != secretWord)
+            {
+                Console.Write("Make a guess: ");
+                guess = Console.ReadLine();
+            }
+            Console.WriteLine("YESSS! You got it!");
+            
+            Console.ReadLine();
+        }
+    }
+}
