9fa6c00782d69d3563824b9d67c2db3c97b4bec8
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-02 21:44:01 +0100

    Added lives and win/lose states

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -9,13 +9,21 @@ namespace MyNamespace
             Console.WriteLine("***Guessing Game***");
             string secretWord = "giraffe";
             string guess = "";
+            int lives = 0;
 
             do
             {
+                if (lives == 4){
+                    Console.WriteLine("YOU LOST...");
+                    break;
+                }
                 Console.Write("Make a Guess: ");
                 guess = Console.ReadLine();
+                lives++;
             } while (guess != secretWord);
+            if (guess == secretWord){
             Console.WriteLine("YESSS! You got it!");
+            }
             
             Console.ReadLine();
         }
