dbbff01186bdd8c17b0ff82e7c80b74b44f71393
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-02 21:00:04 +0100

    Added MadLibsGame

---
+++ MadLibsGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyNamespace 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a color: ");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            string pluralnoun = Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity name: ");
+            string celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
