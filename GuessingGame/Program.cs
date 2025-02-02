7decc2516c62a699d16b81172e98d799f44ac0b3
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-02 21:39:07 +0100

    Updated while->do while

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,14 +6,15 @@ namespace MyNamespace
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***Guessing Game***");
             string secretWord = "giraffe";
-
             string guess = "";
-            while (guess != secretWord)
+
+            do
             {
-                Console.Write("Make a guess: ");
+                Console.Write("Make a Guess: ");
                 guess = Console.ReadLine();
-            }
+            } while (guess != secretWord);
             Console.WriteLine("YESSS! You got it!");
             
             Console.ReadLine();
