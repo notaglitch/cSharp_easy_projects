11002edcca1a971a91cb3d0269aee3d6504415df
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-07 21:49:00 +0100

    Added files

---
+++ StudentGradeManage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentGradeManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of students: ");
+            int numStudents = int.Parse(Console.ReadLine());
+
+            string[] names = new string[numStudents];
+            
+            Console.ReadLine();
+    }
+}
